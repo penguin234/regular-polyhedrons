c5bd5d41fa2672fe8cc44896d444fb4fc592efd5
penguin234 <56291718+penguin234@users.noreply.github.com>
2023-01-16 22:58:35 +0900

    Some progress

--- reqular-polyhedron-validator/rule.js
+++ reqular-polyhedron-validator/rule.js
@@ -4,6 +4,7 @@ function Rule(pointCount, surfaceCount, surfaceAngle) {
   this.surfaceAngle = surfaceAngle;
 }
 
+
 Rule.prototype.ValidatePointCount = function(polyhedron) {
   const pointCount = polyhedron.points.length;
   if (this.pointCount == pointCount) {
@@ -17,3 +18,26 @@ Rule.prototype.ValidatePointCount = function(polyhedron) {
     }
   }
 };
+
+
+Rule.prototype.ValidateSurfaceCount = function(polyhedron) {
+  const surfaceCount = polyhedron.surfaces.length;
+  if (this.surfaceCount == surfaceCount) {
+    return {
+      OK: true
+    }
+  }
+  else {
+    return {
+      Err: { message: 'surfaces should be ' + String(this.pointCount) + ' but it is ' + String(pointCount) }
+    }
+  }
+};
+
+
+Rule.prototype.CheckSideLength = function(surface) {
+  const c = surface.length;
+  let v = Vector.FromPoints(surface[c - 1], surface[0])
+  const l = Math.sqrt(Vector.InnerProduct(v, v));
+  for (let i = 1; i < c; i++) {
+    v = Vector.FromPoints(surface[i - 1], surface[i])
---
+++ reqular-polyhedron-validator/vector.js
@@ -0,0 +1,9 @@
+function Vector() {}
+
+Vector.FromPoints(start, end) {
+  return [end[0] - start[0], end[1] - start[1], end[2] - start[2]];
+};
+
+Vector.InnerProduct(p1, p2) {
+  return p1[0] * p2[0] + p1[1] * p2[1] + p1[2] * p2[2];
+};
